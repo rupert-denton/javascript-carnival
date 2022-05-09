b9b204183d6d24ec091309e97a1b3b249d9288c4
rupert-denton <rupertdenton1@gmail.com>
2022-05-10 10:33:29 +1200

    finished inflate the horn

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,34 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+let unicornImageArray = [
+  './images/unicorn-0.png',
+  './images/unicorn-1.png',
+  './images/unicorn-2.png',
+  './images/unicorn-3.png',
+]
+
+let unicornImages = document.getElementsByTagName('img')
+
+const inflateHorn = function (clickedUnicornId) {
+  let unicornImage = document.getElementById(clickedUnicornId)
+  let lastImage = unicornImageArray[unicornImageArray.length - 1]
+  if (unicornImage.getAttribute('src') === lastImage) {
+    alert(`${unicornImage.id} thanks you!`)
+  } else {
+    let imageNumber = unicornImageArray.indexOf(
+      unicornImage.getAttribute('src')
+    )
+    let nextImage = ++imageNumber
+    unicornImage.setAttribute('src', unicornImageArray[nextImage])
+  }
+}
+
+for (let i = 0; i < unicornImages.length; i++) {
+  unicornImages[i].setAttribute('src', unicornImageArray[0])
+  unicornImages[i].addEventListener('click', function (e) {
+    let clickedUnicornId = e.target.id
+    inflateHorn(clickedUnicornId)
+  })
+}
