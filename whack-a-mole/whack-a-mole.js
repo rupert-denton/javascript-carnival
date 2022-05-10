7a199fca911aa9f94cec43151b2813ccc38ddd8b
rupert-denton <rupertdenton1@gmail.com>
2022-05-10 15:16:49 +1200

    finished whack a mole

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,32 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+let table = document.getElementById('table')
+let cells = document.getElementsByTagName('td')
+let startButton = document.getElementById('start')
+let randomCell
+
+const moleCreator = function () {
+  let img = document.createElement('img')
+  img.setAttribute('src', './mole.PNG')
+  img.setAttribute('id', 'mole')
+  img.classList.add('mole-img')
+  return img
+}
+
+const moleRandomizer = function () {
+  let randomCell = cells[Math.floor(Math.random() * cells.length)]
+  img = moleCreator()
+  randomCell.append(img)
+  randomCell.addEventListener('click', function () {
+    let audio = new Audio('whack-audio.wav')
+    audio.play()
+    document.getElementById('mole').remove()
+    moleRandomizer()
+  })
+}
+
+startButton.addEventListener('click', function () {
+  moleRandomizer()
+})
